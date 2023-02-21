36aa3802a50d5554aae07ed393e589f2807583c0
HelaMo <helamoalla91@gmail.com>
2023-02-21 21:12:56 +0100

    second commit

--- src/main/java/com/mycompany/pidevdr/Appli.java
+++ src/main/java/com/mycompany/pidevdr/Appli.java
@@ -33,16 +33,27 @@ public class Appli {
         //ajout garage methode 1
         //sg.insert(g);
         
-       // GarageC g1=new GarageC("Garage hela","tunis ",98214541,1000,500,600,200,500,300,500,400,1000,2000,500,600,400,100," ",10,"image");
-      //sg.insert(g1);
+        GarageC g1=new GarageC("Garage hela","tunis ",98214541,1000,500,600,200,500,300,500,400,1000,2000,500,600,400,100," ",10,"image");
+      sg.insert(g1);
         
-       //supprimer garage
-       // sg.delete(9);
+      //affichage
+        System.out.println(sg.readAll());
        
-       //update garage
-       /*g1.setId_garage(11);
+       
+      //update garage
+       g1.setId_garage(1);
        g1.setAdresse("menzah 9");
-        sg.update(g1);*/
+        sg.update(g1);
+       
+        //affichage
+        System.out.println(sg.readAll());
+       
+       //supprimer garage
+       sg.delete(1);
+       
+       //affichage
+        System.out.println(sg.readAll());
+       
       
        
        //affichage
@@ -50,11 +61,11 @@ public class Appli {
        // System.out.println(sg.readAll());
        
        ///recherche par id 
-   //System.out.println(sg.readById(11));
+   System.out.println(sg.readById(11));
 
 
  //trier 
-      // System.out.println(sg.sortBy("panne_moteur","ASC"));
+      System.out.println(sg.sortBy("panne_moteur","ASC"));
        
       
       /////////////////Maintenance///////////////////
@@ -64,15 +75,22 @@ public class Appli {
        //Maintenance m1=new Maintenance(2,false,false,false,false,false,true,false,false,true,false,false,false,false,false,"");
       // sm.insert(m1);
        
-     //supprimer maintenance
-       //sm.delete(5);
-        
-         //update maintenance
+      //affichage
+       //System.out.println(sm.readAll());
+       
+        //update maintenance
      /*m1.setId_user(2);
        m1.setPompe_a_eau(true);
         sm.update(m1);*/
-    
-    
+       
+     //affichage
+       //System.out.println(sm.readAll());
+       
+       
+     //supprimer maintenance
+       //sm.delete(5);
+        
+        
      //affichage
        //System.out.println(sm.readAll());
       
